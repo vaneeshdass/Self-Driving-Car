8cc14a5689b20c3f4d34684c21fbefecda122680
Vaneesh Dass <vaneesh.dass03@gmail.com>
2019-02-18 12:27:09 +0530

    canny edges

--- demo.py
+++ demo.py
@@ -1,55 +1,24 @@
+# Do all the relevant imports
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+import cv2
 
-# Read in the image
+# Read in the image and convert to grayscale
+# Note: in the previous example we were reading a .jpg
+# Here we read a .png and convert to 0,255 bytescale
 image = mpimg.imread('highway_lane.jpeg')
+gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
 
-# Grab the x and y sizes and make two copies of the image
-# With one copy we'll extract only the pixels that meet our selection,
-# then we'll paint those pixels red in the original image to see our selection
-# overlaid on the original.
-ysize = image.shape[0]
-xsize = image.shape[1]
-color_select= np.copy(image)
-line_image = np.copy(image)
+# Define a kernel size for Gaussian smoothing / blurring
+kernel_size = 5 # Must be an odd number (3, 5, 7...)
+blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
 
-# Define our color criteria
-red_threshold = 0
-green_threshold = 0
-blue_threshold = 0
-rgb_threshold = [red_threshold, green_threshold, blue_threshold]
+# Define our parameters for Canny and run it
+low_threshold = 50
+high_threshold = 150
+edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
 
-# Define a triangle region of interest (Note: if you run this code,
-# Keep in mind the origin (x=0, y=0) is in the upper left in image processing
-# you'll find these are not sensible values!!
-# But you'll get a chance to play with them soon in a quiz ;)
-left_bottom = [0, 539]
-right_bottom = [900, 300]
-apex = [400, 0]
-
-fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1)
-fit_right = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1)
-fit_bottom = np.polyfit((left_bottom[0], right_bottom[0]), (left_bottom[1], right_bottom[1]), 1)
-
-# Mask pixels below the threshold
-color_thresholds = (image[:,:,0] < rgb_threshold[0]) | \
-                    (image[:,:,1] < rgb_threshold[1]) | \
-                    (image[:,:,2] < rgb_threshold[2])
-
-# Find the region inside the lines
-XX, YY = np.meshgrid(np.arange(0, xsize), np.arange(0, ysize))
-region_thresholds = (YY > (XX*fit_left[0] + fit_left[1])) & \
-                    (YY > (XX*fit_right[0] + fit_right[1])) & \
-                    (YY < (XX*fit_bottom[0] + fit_bottom[1]))
-# Mask color selection
-color_select[color_thresholds] = [0,0,0]
-# Find where image is both colored right and in the region
-line_image[~color_thresholds & region_thresholds] = [255,0,0]
-
-# Display our two output images
-plt.imshow(color_select)
-plt.imshow(line_image)
-
-# uncomment if plot does not display
+# Display the image
+plt.imshow(edges, cmap='Greys_r')
 plt.show()
