a789744edd6625cf4e4a517fc57ab1f8c11600f3
Vaneesh Dass <vaneesh.dass03@gmail.com>
2019-02-21 16:28:10 +0530

    Finding & drawing corners using opencv on chess image.

--- demo.py
+++ demo.py
@@ -1,44 +1,25 @@
-# Do relevant imports
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-# Read in and grayscale the image
-image = mpimg.imread('highway_lane.jpeg')
-gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+# prepare object points
+nx = 8#TODO: enter the number of inside corners in x
+ny = 6#TODO: enter the number of inside corners in y
 
-# Define a kernel size and apply Gaussian smoothing
-kernel_size = 5
-blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+# Make a list of calibration images
+fname = 'calibration_test.png'
+img = cv2.imread(fname)
 
-# Define our parameters for Canny and apply
-low_threshold = 50
-high_threshold = 150
-masked_edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+# Convert to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Define the Hough transform parameters
-# Make a blank the same size as our image to draw on
-rho = 1
-theta = np.pi/180
-threshold = 1
-min_line_length = 10
-max_line_gap = 1
-line_image = np.copy(image)*0 #creating a blank to draw lines on
+# Find the chessboard corners
+ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
 
-# Run Hough on edge detected image
-lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]),
-                            min_line_length, max_line_gap)
-
-# Iterate over the output "lines" and draw lines on the blank
-for line in lines:
-    for x1,y1,x2,y2 in line:
-        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
-
-# Create a "color" binary image to combine with line image
-
-color_edges = np.dstack((masked_edges, masked_edges, masked_edges))
-# Draw the lines on the edge image
-combo = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0)
-plt.imshow(combo)
-plt.show()
+# If found, draw corners
+if ret == True:
+    # Draw and display the corners
+    cv2.drawChessboardCorners(img, (nx, ny), corners, ret)
+    plt.imshow(img)
+    plt.show()
