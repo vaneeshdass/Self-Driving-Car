08f3af5f8a0efc35607214efd464fac5a7cfa608
Vaneesh Dass <vaneesh.dass03@gmail.com>
2019-02-25 09:28:23 +0530

    Magnitude of the Gradient sobel example

--- demo.py
+++ demo.py
@@ -4,48 +4,40 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import pickle
 
-# Read in an image and grayscale it
-image = cv2.imread('signs_vehicles_xygrad.png')
+# Read in an image
+image = mpimg.imread('signs_vehicles_xygrad.png')
 
 
-# Define a function that applies Sobel x or y,
-# then takes an absolute value and applies a threshold.
-# Note: calling your function with orient='x', thresh_min=5, thresh_max=100
-# should produce output like the example image shown above this quiz.
-def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
-    # Apply the following steps to img
-    # 1) Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Define a function that applies Sobel x and y,
+# then computes the magnitude of the gradient
+# and applies a threshold
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Take both Sobel x and y gradients
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Calculate the gradient magnitude
+    gradmag = np.sqrt(sobelx**2 + sobely**2)
+    # Rescale to 8 bit
+    scale_factor = np.max(gradmag)/255
+    gradmag = (gradmag/scale_factor).astype(np.uint8)
+    # Create a binary image of ones where threshold is met, zeros otherwise
+    binary_output = np.zeros_like(gradmag)
+    binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
 
-    # 2) Take the derivative in x or y given orient = 'x' or 'y'
-    # 3) Take the absolute value of the derivative or gradient
-    if orient == 'x':
-        sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
-
-
-    if orient == 'y':
-        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
-
-    abs_sobel = np.absolute(sobel)
-
-    # 4) Scale to 8-bit (0 - 255) then convert to type = np.uint8
-    scaled_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
-    # 5) Create a mask of 1's where the scaled gradient magnitude
-    # is > thresh_min and < thresh_max
-    # 6) Return this mask as your binary_output image
-    sbinary = np.zeros_like(scaled_sobel)
-    sbinary[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
-    return sbinary
+    # Return the binary image
+    return binary_output
 
 
 # Run the function
-grad_binary = abs_sobel_thresh(image, orient='y', thresh_min=20, thresh_max=100)
+mag_binary = mag_thresh(image, sobel_kernel=3, mag_thresh=(30, 100))
 # Plot the result
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
 f.tight_layout()
 ax1.imshow(image)
 ax1.set_title('Original Image', fontsize=50)
-ax2.imshow(grad_binary, cmap='gray')
-ax2.set_title('Thresholded Gradient', fontsize=50)
+ax2.imshow(mag_binary, cmap='gray')
+ax2.set_title('Thresholded Magnitude', fontsize=50)
 plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
 f.show()
