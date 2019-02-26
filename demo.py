30ed176588382604027909140cbc7527fcdc540e
Vaneesh Dass <vaneesh.dass03@gmail.com>
2019-02-26 12:29:41 +0530

    Color and Gradient combined(more).

--- demo.py
+++ demo.py
@@ -1,50 +1,45 @@
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+image = mpimg.imread('bridge_shadow.jpg')
 
 
-img = mpimg.imread("test1.jpg")
-# Convert to HLS color space and separate the S channel
-# Note: img is the undistorted image
-hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-s_channel = hls[:,:,2]
+# Edit this function to create your own pipeline.
+def pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
+    img = np.copy(img)
+    # Convert to HLS color space and separate the V channel
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    l_channel = hls[:, :, 1]
+    s_channel = hls[:, :, 2]
+    # Sobel x
+    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0)  # Take the derivative in x
+    abs_sobelx = np.absolute(sobelx)  # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8(255 * abs_sobelx / np.max(abs_sobelx))
 
-# Grayscale image
-# NOTE: we already saw that standard grayscaling lost color information for the lane lines
-# Explore gradients in other colors spaces / color channels to see what might work better
-gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Threshold x gradient
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
 
-# Sobel x
-sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0) # Take the derivative in x
-abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
-scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+    # Threshold color channel
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+    # Stack each channel
+    color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
+    return color_binary
 
-# Threshold x gradient
-thresh_min = 20
-thresh_max = 100
-sxbinary = np.zeros_like(scaled_sobel)
-sxbinary[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
 
-# Threshold color channel
-s_thresh_min = 170
-s_thresh_max = 255
-s_binary = np.zeros_like(s_channel)
-s_binary[(s_channel >= s_thresh_min) & (s_channel <= s_thresh_max)] = 1
+result = pipeline(image)
 
-# Stack each channel to view their individual contributions in green and blue respectively
-# This returns a stack of the two binary images, whose components you can see as different colors
-color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
+# Plot the result
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
 
-# Combine the two binary thresholds
-combined_binary = np.zeros_like(sxbinary)
-combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=40)
 
-# Plotting thresholded images
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
-ax1.set_title('Stacked thresholds')
-ax1.imshow(color_binary)
-
-ax2.set_title('Combined S channel and gradient thresholds')
-ax2.imshow(combined_binary, cmap='gray')
+ax2.imshow(result)
+ax2.set_title('Pipeline Result', fontsize=40)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
 f.show()
