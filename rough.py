ae1a0b9b7c04ab72146b9158c3ee45c35fa3aade
Vaneesh Dass <vaneesh.dass03@gmail.com>
2019-02-26 12:25:30 +0530

    Color and Gradient combined.

--- rough.py
+++ rough.py
@@ -1,4 +1,34 @@
-import pickle
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+
+# Read in an image, you can also try test1.jpg or test4.jpg
+image = mpimg.imread('test6.jpg')
+
+
+# Define a function that thresholds the S-channel of HLS
+def hls_select(img, thresh=(0, 255)):
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    s_channel = hls[:, :, 2]
+    binary_output = np.zeros_like(s_channel)
+    binary_output[(s_channel > thresh[0]) & (s_channel <= thresh[1])] = 1
+    return binary_output
+
+
+hls_binary = hls_select(image, thresh=(90, 255))
+
+# Plot the result
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(hls_binary, cmap='gray')
+ax2.set_title('Thresholded S', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show()
+
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,13 +40,13 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Calculate the derivative in the xx direction (the 1, 0 at the end denotes xx direction)
 sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
-#Calculate the derivative in the yy direction (the 0, 1 at the end denotes yy direction)
+# Calculate the derivative in the yy direction (the 0, 1 at the end denotes yy direction)
 sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
 
-#this absolute fns turn all negative values into positive
+# this absolute fns turn all negative values into positive
 abs_sobelx = np.absolute(sobelx)
 
-scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+scaled_sobel = np.uint8(255 * abs_sobelx / np.max(abs_sobelx))
 
 thresh_min = 20
 thresh_max = 100
@@ -24,4 +54,3 @@ sxbinary = np.zeros_like(scaled_sobel)
 sxbinary[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
 plt.imshow(sxbinary, cmap='gray')
 plt.show()
-
