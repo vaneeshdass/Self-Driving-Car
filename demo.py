cf29efd3ff87fcaf30a5b2d6ac29b24ac62a31fc
Vaneesh Dass <vaneesh.dass03@gmail.com>
2019-02-27 18:24:33 +0530

    finding the radius of curvature based on previous commit but in real word space (in meter's) .

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def generate_data():
+def generate_data(ym_per_pix, xm_per_pix):
     '''
     Generates fake data to use for calculating lane curvature.
     In your own project, you'll ignore this function and instead
@@ -25,34 +25,41 @@ def generate_data():
     rightx = rightx[::-1]  # Reverse to match top-to-bottom in y
 
     # Fit a second order polynomial to pixel positions in each fake lane line
-    left_fit = np.polyfit(ploty, leftx, 2)
-    right_fit = np.polyfit(ploty, rightx, 2)
+    # Fit new polynomials to x,y in world space
+    left_fit_cr = np.polyfit(ploty * ym_per_pix, leftx * xm_per_pix, 2)
+    right_fit_cr = np.polyfit(ploty * ym_per_pix, rightx * xm_per_pix, 2)
 
-    return ploty, left_fit, right_fit
+    return ploty, left_fit_cr, right_fit_cr
 
 
-def measure_curvature_pixels():
+def measure_curvature_real():
     '''
-    Calculates the curvature of polynomial functions in pixels.
+    Calculates the curvature of polynomial functions in meters.
     '''
+    # Define conversions in x and y from pixels space to meters
+    ym_per_pix = 30 / 720  # meters per pixel in y dimension
+    xm_per_pix = 3.7 / 700  # meters per pixel in x dimension
+
     # Start by generating our fake example data
     # Make sure to feed in your real data instead in your project!
-    ploty, left_fit, right_fit = generate_data()
+    ploty, left_fit_cr, right_fit_cr = generate_data(ym_per_pix, xm_per_pix)
 
     # Define y-value where we want radius of curvature
     # We'll choose the maximum y-value, corresponding to the bottom of the image
     y_eval = np.max(ploty)
 
     # Calculation of R_curve (radius of curvature)
-    left_curverad = ((1 + (2 * left_fit[0] * y_eval + left_fit[1]) ** 2) ** 1.5) / np.absolute(2 * left_fit[0])
-    right_curverad = ((1 + (2 * right_fit[0] * y_eval + right_fit[1]) ** 2) ** 1.5) / np.absolute(2 * right_fit[0])
+    left_curverad = ((1 + (2 * left_fit_cr[0] * y_eval * ym_per_pix + left_fit_cr[1]) ** 2) ** 1.5) / np.absolute(
+        2 * left_fit_cr[0])
+    right_curverad = ((1 + (2 * right_fit_cr[0] * y_eval * ym_per_pix + right_fit_cr[1]) ** 2) ** 1.5) / np.absolute(
+        2 * right_fit_cr[0])
 
     return left_curverad, right_curverad
 
 
-# Calculate the radius of curvature in pixels for both lane lines
-left_curverad, right_curverad = measure_curvature_pixels()
+# Calculate the radius of curvature in meters for both lane lines
+left_curverad, right_curverad = measure_curvature_real()
 
-print(left_curverad, right_curverad)
-# Should see values of 1625.06 and 1976.30 here, if using
+print(left_curverad, 'm', right_curverad, 'm')
+# Should see values of 533.75 and 648.16 here, if using
 # the default `generate_data` function with given seed number
